0cbfbff3f6a5291f9e33ea01eec1f61864fc47eb
Rifat0001 <mahmudrifat764@gmail.com>
2023-07-26 20:54:49 +0600

    done

--- src/Components/HomeComponents/FindProperty.jsx
+++ src/Components/HomeComponents/FindProperty.jsx
@@ -2,24 +2,24 @@ import { FaSearch } from "react-icons/fa";
 import "./HomeComponents.css";
 const FindProperty = () => {
   return (
-    <div className="bg-blue-100 pt-16 p-4 pb-16 rounded-md">
+    <div className="bg-blue-100 pt-16 p-2 pb-16 rounded-md">
       <div className="max-w-[2150px] mx-auto xl:px-40 md:px-10 sm:px-2 px-4">
         {/* <h3 className="text-3xl font-bold text-center text-black mb-4">
           Find Your Property
         </h3> */}
-        <div className="flex">
-          <div className="ms-4">
-            <button className="btn choose  text-black border-none drop-shadow-lg me-2 bg-white">
+        <div className="flex md:flex-row flex-col ">
+          <div className="md:ms-4 md:mb-0 mb-6 flex ms-0 ">
+            <button className="btn choose md:w-[70px] w-[180px]  text-black border-none drop-shadow-lg me-2 bg-white">
               Buy
             </button>
-            <button className="btn choose text-black border-none drop-shadow-lg me-2 bg-white">
+            <button className="btn choose  md:w-[70px] w-[180px] text-black border-none drop-shadow-lg me-2 bg-white">
               Rent
             </button>
           </div>
           <div>
             <select
               name="cars"
-              className="text-black border-none drop-shadow-lg w-[200px] h-[48px] rounded-md ms-4 ps-3 font-semibold "
+              className="text-black md:mb-0 mb-6 border-none drop-shadow-lg md:w-[200px] w-full h-[48px] rounded-md ms-0 md:ms-4 ps-3 font-semibold "
               id="cars"
             >
               <option value="volvo">Homes</option>
@@ -28,11 +28,11 @@ const FindProperty = () => {
               <option value="audi">New Homes</option>
             </select>
           </div>
-          <div className="flex items-center gap-4 drop-shadow-lg ms-20">
+          <div className="flex items-center gap-4 drop-shadow-lg ms-0 md:ms-20">
             <div className="flex items-center gap-4 h-full bg-white rounded-md">
               <FaSearch className="text-2xl ms-2"></FaSearch>
               <input
-                className="search border w-[550px]  rounded-md text-black h-full border-none"
+                className="search border md:w-[550px] w-[217px]  rounded-md text-black h-12 md:h-full border-none"
                 type="text"
               />
             </div>
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import './NavBar.css'
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
-            <div className="sm:navbar-start md:hidden">
+            <div className="sm:navbar-start md:hidden ">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="btn btn-primary btn-outline btn-sm lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu bg-black menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>Item 1</a></li>
                         <li>
                             <a>Parent</a>
@@ -20,7 +20,7 @@ const Navbar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-primary italic font-bold text-[22px]">Real State</a>
             </div>
             <div className="navbar-start hidden text-black font-semibold  lg:flex">
                 <ul className="menu ms-[-30px] main menu-horizontal px-1">
@@ -31,7 +31,7 @@ const Navbar = () => {
                     <li><a>Blog</a></li>
                 </ul>
             </div>
-            <div className="navbar-end gap-6">
+            <div className="navbar-end md:gap-6 gap-2" >
                 <div className='border border-primary p-2 rounded-full'>
                     <FaUser className='text-primary'></FaUser>
                 </div>
