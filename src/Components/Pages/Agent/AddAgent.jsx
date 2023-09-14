69b7ecb4e69a6f268f38f4738a5dc3ee48730d07
Rifat0001 <mahmudrifat764@gmail.com>
2023-09-15 03:06:49 +0600

    allhamdulillah

--- src/Components/Pages/Agent/AddAgent.jsx
+++ src/Components/Pages/Agent/AddAgent.jsx
@@ -9,7 +9,7 @@ const AddAgent = () => {
                 <button className="btn btn-gradient">Search</button>
             </div>
             {/* agent card  */}
-            <div className="card w-96 card-compact text-black bg-base-100 shadow-xl rounded-md">
+            <div className="card card-compact text-black bg-base-100 shadow-xl rounded-md">
                 <div className="relative">
                     <figure>
 
