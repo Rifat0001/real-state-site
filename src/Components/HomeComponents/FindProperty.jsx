42c12870e4a46a39007a49d999d39f4297106d69
Rifat0001 <mahmudrifat764@gmail.com>
2023-09-15 03:00:43 +0600

    donoe

--- src/Components/HomeComponents/FindProperty.jsx
+++ src/Components/HomeComponents/FindProperty.jsx
@@ -3,44 +3,33 @@ import "./HomeComponents.css";
 const FindProperty = () => {
   return (
     <div className=" ">
-      <div className="max-w-[2150px] mt-8 mx-auto xl:px-36 md:px-10 sm:px-2 px-4">
-        {/* <h3 className="text-3xl font-bold text-center text-black mb-4">
-          Find Your Property
-        </h3> */}
-        <div className="flex bg-white drop-shadow-lg  py-8 rounded-md  ps-4 md:flex-row flex-col ">
-          <div className="flex justify-center">
-            <select
-              name="cars"
-              className="text-black md:mb-0 mb-6 border-black drop-shadow border md:w-[200px] w-[20.5rem] h-[48px] rounded-md  md:ms-4  p-3 font-semibold "
-              id="cars"
-            >
-              <option value="Buy">Buy</option>
-              <option value="Rent">Rent</option>
+      <div className="max-w-[2150px] mt-8 mx-auto md:px-36 sm:px-2 px-4">
+        <div className="grid md:grid-cols-4 grid-cols-1 bg-white gap-10 drop-shadow-lg px-4 md:px-20 py-8 rounded-md">
+          <div className="form-control w-full max-w-xs">
+            <select className="select w-full border text-black border-black">
+              <option selected>
+                Rent
+              </option>
+              <option>Sales</option>
             </select>
-
           </div>
-          <div className="flex justify-center">
-            <select
-              name="cars"
-              className="text-black md:mb-0 mb-6 border border-black drop-shadow  md:w-[200px] w-[20.5rem] h-[48px] rounded-md  md:ms-4  p-3 font-semibold"
-              id="cars"
-            >
-              <option value="volvo">Homes</option>
-              <option value="saab">Room</option>
-              <option value="opel">Garage</option>
-              <option value="audi">New Homes</option>
+          <div className="form-control w-full max-w-xs">
+            <select className="select w-full border text-black border-black">
+              <option selected>
+                Home
+              </option>
+              <option>Office</option>
+              <option>Appartment</option>
+
             </select>
-
           </div>
-          <div className="flex justify-center items-center gap-2 md:gap-4 drop-shadow  ms-0 md:ms-20">
-            <div className="flex border items-center gap-4 h-full rounded-md">
-
-              <input
-                className="search border md:w-[560px] w-[185px]text-black border-black rounded-md h-12 md:h-full ps-4 text-black border-none bg-white"
-                type="text" placeholder="Enter your location "
-              />
+          <div className="form-control col-span-2">
+            <div className="input-group w-full">
+              <input type="text" placeholder="Search your location" className="input w-full border-black input-bordered" />
+              <button className="btn btn-gradient">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              </button>
             </div>
-            <button className="btn btn-gradient border-none drop-shadow-md ">Search</button>
           </div>
         </div>
       </div>
